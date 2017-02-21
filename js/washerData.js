81c1ab8105b9b22952d0f93dc14714387d4cea41
Stephen Weber <stephen.weber3@gmail.com>
2017-02-20 22:58:03 -0500

    Updated position of itemprice

--- js/washerData.js
+++ js/washerData.js
@@ -6,8 +6,7 @@
                 '<li class="itembrand col-md-9">' + data.productList[0].brand + '</li>' +
                 '<li class="itemdescription col-md-9">' + data.productList[0].description + '</li>' +
                 '<li class="cart col-md-9"><button type="button" class="btn btn-primary add-to-cart primarybutton">' + 'Add To Cart' + '</button></li>' + 
-                '<li class="itemprice col-md-9"> <p>$' + data.productList[0].pricing.price.retail + '</p></li></ul>' + 
-                '');
+                '<li class="itemprice col-md-9"> <p>$' + data.productList[0].pricing.price.retail + '</p></li></ul>');
 
             for (var i = 0; i < data.productList.length; i++) {
                 $('#container').append('<div class="productBox-' + i + ' col-md-4"></div>');
@@ -15,7 +14,7 @@
                 $(' .productBox-' + i + ' ').append('<ul><li class="itemimage text-center"><img class="img-rounded" src=' + data.productList[i].imageUrls.sm + '></li>' +
                     '<li class="itemBrand text-center">' + data.productList[i].brand + '</li>' +
                     '<li class="itemdescription text-center">' + data.productList[i].description + '</li>' +
-                    '<li class="itemprice text-center">$' + data.productList[i].pricing.price.retail + '</li>' +
+                    '<li class="itemprice text-center"> <p>$' + data.productList[i].pricing.price.retail + '</p></li>' +
                     '<li class="itempreview text-center"><button type="button" class="btn btn-primary">' + 'View More' + '</button></li></ul>');
             }
 
@@ -29,7 +28,7 @@
                 $('#hero .itemimage img').attr('src', image);
                 $('#hero li.itemBrand').text(brand);
                 $('#hero li.itemdescription').text(description);
-                $('#hero li.itemprice').text(price);
+                $('#hero li.itemprice p').text(price);
             });
 
             $('img').hover(function() {
@@ -42,7 +41,7 @@
                 $('#hero .itemimage img').attr('src', image);
                 $('#hero li.itemBrand').text(brand);
                 $('#hero li.itemdescription').text(description);
-                $('#hero li.itemprice.p').text(price);
+                $('#hero li.itemprice p').text(price);
             });
 
 
